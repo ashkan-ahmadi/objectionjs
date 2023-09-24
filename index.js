8e4710c262db63506e04fb5424b5229ff6732c0e
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2023-09-24 18:20:39 +0200

    Update

--- index.js
+++ index.js
@@ -12,8 +12,16 @@ const Orders = require('./db/models/orders')
 const OrderStatuses = require('./db/models/orderStatuses')
 
 const foo = async () => {
-  const p = await Orders.query().findById(1).withGraphFetched('product').withGraphFetched('user').withGraphFetched('status')
-  // console.log(p)
+  const orders = await Orders.query().withGraphFetched('product').withGraphFetched('user').withGraphFetched('status').where({ userId: 1 })
+
+  //  not sure why this returns false
+  if (!(orders instanceof Orders)) {
+    console.log('NOT')
+  } else {
+    console.log('all good')
+  }
+
+  console.log(orders)
 }
 
 foo()
